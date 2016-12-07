f7651d43d1c08b011dce180e6046d532ebcd7613
JETYIN <494699974@qq.com>
2016-12-07 13:02:44 +0800

    change   location so

--- app/src/main/java/com/work/ndkdemo/MainActivity.java
+++ app/src/main/java/com/work/ndkdemo/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
     static {
         System.loadLibrary("MyJni");
     }
-
+/**将在build-ndk中的.so包复制到新建的lib包下，可以删除main下面的jni包依旧运行**/
     /**
      * 创建本地方法
      **/
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-        Log.e("getStringFromNative",getStringFromNative());
+        Log.e("getStringFromNative", getStringFromNative());
     }
 
     public void onClick(View view) {
